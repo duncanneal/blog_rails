7533635c0bf23f3ce5af0be1d573e516a5316b22
Duncan Neal <duncan.neal@gmail.com>
2015-07-01 12:26:53 -0400

    test push

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,8 @@ class Post < ActiveRecord::Base
   validates :body, presence: true
 
 
+
+
   def all_tags=(names)
     self.tags = names.split(",").map do |name|
       Tag.where(name: name.strip).first_or_create!
